c49ebbf1727d5cefc49532c9de18309dc342578c
Michael Fairchild <mfairchild365@gmail.com>
2014-03-13 09:25:31 -0500

    Remove debug code

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -2,6 +2,5 @@
     The W3C HTML validator checks for html validation errors.  It will only report errors and ignores notices.  HTML errors are due to invalid HTML markup in your pages.  They may cause inconsistent rendering and behavior between browsers.
 </p>
 <p>
-    <?php var_dump($context->options->getRawObject()); ?>
     To find and fix these errors, you can run your page though the <a href="<?php echo $context->options['service_url'] ?>">W3C HTML validator</a>.
 </p>
