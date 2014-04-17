a5a021cc933c7e9b5e593faaac742fe3bb8158b4
Michael Fairchild <mfairchild365@gmail.com>
2014-04-17 09:18:04 -0500

    make the link scan the checked page

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -2,5 +2,12 @@
     The W3C HTML validator checks for html validation errors.  It will only report errors and ignores notices.  HTML errors are due to invalid HTML markup in your pages.  They may cause inconsistent rendering and behavior between browsers.
 </p>
 <p>
-    To find and fix these errors, you can run your page though the <a href="<?php echo $context->options['service_url'] ?>">W3C HTML validator</a>.
+    <?php
+    $url = $context->options['service_url'];
+    if (isset($parent) && $parent->context->getRawObject() instanceof \SiteMaster\Core\Auditor\Site\Page\MetricGrade) {
+        $page = $parent->context->getPage();
+        $url .= '?uri=' . urlencode($page->uri);
+    }
+    ?>
+    To find and fix these errors, you can run your page though the <a href="<?php echo $url ?>">W3C HTML validator</a>.
 </p>
