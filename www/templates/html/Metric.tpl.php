aaaedf36fb9f2b5025f605e8691ec4185fd4dafe
Michael Fairchild <mfairchild365@gmail.com>
2014-08-29 16:08:07 -0500

    Remove 'check' from the URL if there is no page to check.

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -7,6 +7,8 @@
     if (isset($parent) && $parent->context->getRawObject() instanceof \SiteMaster\Core\Auditor\Site\Page\MetricGrade) {
         $page = $parent->context->getPage();
         $url .= '?uri=' . urlencode($page->uri);
+    } else {
+        $url = str_replace('check', '', $url);
     }
     ?>
     To find and fix these errors, you can run your page though the <a href="<?php echo $url ?>">W3C HTML validator</a>.
